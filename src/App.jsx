c60288f0e151693f688ddfc056f61fb98dd756e0
Rafiw Idrissu <86615946+Idrissu-Rafiu@users.noreply.github.com>
2023-06-18 17:04:02 -0700

    final changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useRef } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [color, setColor] = useState("white");
+  const inputRef = useRef(null);
+
+  function handleColor() {
+    const { value } = inputRef.current;
+    if (value == null) {
+      return;
+    }
+    setColor(value);
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container" style={{ backgroundColor: color }}>
+      <input
+        type="text"
+        ref={inputRef}
+        placeholder="add your color without space"
+      />
+      <button onClick={handleColor}>Display Color</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
